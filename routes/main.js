693fb0f8580072f5230e6d89ce8ab04164c2eb21
Fadhlursz <78072515+Fadhlursz@users.noreply.github.com>
2021-03-17 22:53:01 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,9 +16,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'NotBot',
+            namaowner: 'Fadhlur',
+            instagram: 'sul.lur',
             youtube : 'Gak Punya'
         }
     }
